2765c63474a06d88e43d030a8b9ba86f0f69a390
rj1tampa <rj1tampa@gmail.com>
2016-08-15 19:35:31 +0530

    repository folder
    repository java class

---
+++ Config.java
@@ -0,0 +1,10 @@
+package repository;
+
+public class Config {
+	
+	//Excel Sheet name
+	final public static String excelName="UserInputData.xlsx";
+	final public static String excelPath="E:\\BSSM3.NEWFRAMEWORK/Automation/excel/";
+	//key name in property file
+	final public static String currentURL="currentURL";
+}
---
+++ Webobjects.java
@@ -0,0 +1,16 @@
+package repository;
+
+public class Webobjects {
+
+	//Web Object Properties
+	final public static String userName="username";
+	final public static String pwd="pwd";
+	final public static String loginButton="loginButton";
+	final public static String Error="//span[contains(text(),'Username or Password is invalid. Please try again.')]";
+	final public static String EnterTimeTrackText="//td[contains(text(),'Enter Time-Track')]";
+	final public static String dropdownArrow="//div[contains(@class,'popup_menu_arrow')])[3]";
+	final public static String actiTIMElink="bout actiTIME";
+	final public static String buildNumber="//span[contains(text(),'(build 29461)')]";
+	final public static String homeScreenAssertion="Enter Time-Track";
+	final public static String build="(build 29461)";
+}
